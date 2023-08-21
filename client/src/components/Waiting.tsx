936637b18668e9942ae6366e763f3f8dec8eeaa3
Nitin Kumar <nitinkumar@Nitins-MacBook-Air.local>
2023-08-22 00:12:21 +0530

    Initial Commit

---
+++ client/src/components/Waiting.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Waiting = () => {
+  return (
+    <div className="waiting-wrapper">
+      <div className="cog">
+        <img src="loading.gif" alt="" />
+      </div>
+
+      <div className="title-small wait">WAITING FOR THE ELECTION TO START</div>
+    </div>
+  );
+};
+
+export default Waiting;
